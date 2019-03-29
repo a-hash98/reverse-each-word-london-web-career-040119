7bee5fc9ef969c1ac99a9ef2b78af15d812acd2f
Ay Has <ahashim983@yahoo.com>
2019-03-29 00:26:32 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence)
   s = sentence.reverse
   s_to_array = s.split(/,/)
-  s_to_array.collect {|s| s}
+  s_to_array.reverse.collect {|s| s}
 end
 #puts reverse_each_word("The cat sat on the mat")
