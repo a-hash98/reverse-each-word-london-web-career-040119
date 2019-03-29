6a3341459543250817b6d446ed03a31c17d38d63
Ay Has <ahashim983@yahoo.com>
2019-03-29 00:15:53 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,9 @@
+def reverse_each_word(sentence)
+  s_collector = String.new
+  array = sentence.to_a
+  array.each do |element|
+    s_collector.concat("#{element.reverse} ")
+  end
+  return s_collector
+end
+  
