60efdb76b360cd14c5c92884b55acc0865ad47cf
Ay Has <ahashim983@yahoo.com>
2019-03-29 00:20:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,8 @@ def reverse_each_word(sentence)
   s_collector = String.new
   array = sentence.chars
   array.each do |element|
-    s_collector.concat("#{element.reverse} ")
+    puts element.reverse
+  
   end
   return s_collector
 end
