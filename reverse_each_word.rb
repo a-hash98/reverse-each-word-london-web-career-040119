74757ceedc67da1d426e4807e91fc0f24885144c
Ay Has <ahashim983@yahoo.com>
2019-03-29 00:28:36 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,8 @@
 def reverse_each_word(sentence)
   s_to_array = s.split(/,/)
+  puts s_to_array
   s_to_array.collect {|s| s.reverse}
 end
 #puts reverse_each_word("The cat sat on the mat")
+
+reverse_each_word("cat on a mat")
